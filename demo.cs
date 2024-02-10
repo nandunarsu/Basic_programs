0c3ef6cb8b4c3498ce5635e016fdd20bffd304e5
nandunarsu <nandinink1947@gmail.com>
2024-02-10 12:06:10 +0530

    added Demo code

---
+++ demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+    class demo
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Its dubbranch code");
+        }
+    }
+}
