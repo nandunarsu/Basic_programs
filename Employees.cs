ac17d7efba75185a1ebdd304ab592e05e98fb911
nandunarsu <nandinink1947@gmail.com>
2024-02-20 15:52:30 +0530

    Hashset programs

---
+++ Employees.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetDemo
+{
+    class Employees :IEquatable <Employees>
+    {
+        public int Employeeid {  get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+        public Employees(int id, string name, string department)
+        {
+            Employeeid = id;
+            Name = name;
+            Department = department;
+        }
+
+        public bool Equals(Employees other)
+        {
+            if (other is null) return false;
+            return  this.Name.Equals(other.Name) ;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+            //return HashCode.Combine(this.Employeeid, this.Name);
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<Employees> employees = new HashSet<Employees>();
+
+
+            employees.Add(new Employees(1, "Nandini", "IT" ));
+            employees.Add(new Employees(2, "Nandu", "ISE"));
+            employees.Add(new Employees(1, "Nandita", "CSE"));
+            employees.Add(new Employees(4, "Nanlini", "Mech"));
+
+            foreach (Employees employee in employees)
+            {
+                Console.WriteLine($"{employee.Employeeid},{employee.Name},{employee.Department}");
+            }
+
+            
+        }
+    }
+}
+
+
